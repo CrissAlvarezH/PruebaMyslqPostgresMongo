53b3d422d5056340db965bc11e2e343840637c01
Cristia Alvarez H <alvarezhernandezcristian1@gmail.com>
2018-11-30 09:39:33 -0500

    Agrego soporte a concurrencia en las consultas

--- src/app.js
+++ src/app.js
@@ -5,46 +5,135 @@ function getHora() {
     return date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds() + ":" + date.getMilliseconds();
 }
 
+let bdParam = process.argv[2];
+let accion = process.argv[3];
 
-/* [ INICIO ] SCRIPT PARA POSTGRESQL */
+switch (bdParam) {
+    case 'postgres':
 
-const { Client } = require('pg'); // requerimos postgresql
+        const { Client } = require('pg'); // requerimos postgresql
 
-// Creamos el cliente para la conexión con Postgres
-const cliente = new Client({
-    user: 'prueba',
-    host: 'localhost',
-    database: 'prueba',
-    password: 'prueba',
-    port: 5432
-});
+        // Creamos el cliente para la conexión con Postgres
+        const cliente = new Client({
+            user: 'prueba',
+            host: 'localhost',
+            database: 'prueba',
+            password: 'prueba',
+            port: 5432
+        });
 
-let llenarTablaPruebas = async (cantidad) => {
+        // Ejecutamos la acción respectiva con la base de datos Postgress
+        switch (accion) {
+            case 'insertar':
+                let cantidad = process.argv[4];
+                let forma = process.argv[5];
 
-    for ( let i = 0; i < cantidad; i++ ) {
-        await cliente.query("INSERT INTO prueba (texto1, texto2, texto3, texto4, texto5) VALUES ('texto1', 'texto2', 'texto3', 'texto4', 'texto5');");
-    }
+                switch (forma) {
+                    case 'secuencial':
 
+                        let insercionPgSecuencial = async () => {
+                            await cliente.connect();
+                        
+                            console.log( ` - Empezó inserción:  ${ getHora() } ` );
+        
+                            for ( let i = 0; i < cantidad; i++ ) {
+                                await cliente.query("INSERT INTO prueba (texto1, texto2, texto3, texto4, texto5) VALUES ('texto1', 'texto2', 'texto3', 'texto4', 'texto5');");
+                            }
+                            
+                            console.log( ` - Terminó inserción: ${ getHora() } ` );
+        
+                            await cliente.end();
+                        
+                            return ' ==== Proceso secuencial terminado ==== ';
+                        }
+                        
+                        insercionPgSecuencial()
+                            .then( res => console.log(res) )
+                            .catch( err => console.log(` >>>>>>> Error ${err}`) );
+
+                        break;
+                    case 'concurrente':
+
+                        let insercionPgConcurrente = async () => {
+                            let procesosTerminados = 0;
+                            let procesosTerminadosMal = 0;
+
+                            await cliente.connect();
+                        
+                            console.log( ` - Empezó inserción:  ${ getHora() } ` );
+        
+                            for ( let i = 0; i < cantidad; i++ ) {
+                                cliente.query("INSERT INTO prueba (texto1, texto2, texto3, texto4, texto5) VALUES ('texto1', 'texto2', 'texto3', 'texto4', 'texto5');")
+                                    .then( resBd => {
+                                        procesosTerminados++;
+
+                                        if ( procesosTerminados == cantidad ) return console.log(` - Terminó el ultimo proceso con ${procesosTerminadosMal} fallas:  ${ getHora() }`);
+                                    })
+                                    .catch( errBd => {
+                                        procesosTerminados++;
+                                        procesosTerminadosMal++;
+
+                                        if ( procesosTerminados == cantidad ) return console.log(` - Terminó el ultimo proceso con ${procesosTerminadosMal} fallas:  ${ getHora() }`);
+                                    });
+                            }
+                                    
+                            // await cliente.end();
+                        
+                            return ' ==== Proceso de lanzar inserciones asincronas terminado ==== ';
+                        }
+                        
+                        insercionPgConcurrente()
+                            .then( res => console.log(res) )
+                            .catch( err => console.log(` >>>>>>> Error ${err}`) );
+
+                        break;
+                    default:
+                        console.log(` ERROR: parametro ${forma} no soportado `);
+                }
+
+                break;
+
+            case 'consultar':
+
+                break;
+            case 'limpiar':
+
+                let limpiarBdPg = async () => {
+                    await cliente.connect();
+
+                    await cliente.query('DELETE FROM prueba');
+
+                    await cliente.end();
+
+                    return ' ==== Limpieza terminada ==== '
+                }
+
+                limpiarBdPg()
+                    .then( res => console.log(res) )
+                    .catch( err => console.log(` >>>>>>> Error ${err}`) );
+                break;
+            default:
+                console.log(` ERROR: parametro ${accion} no soportado `);        
+        }
+
+        break;
+    case 'mysql':
+
+        switch (accion) {
+            case 'insertar':
+
+                break;
+            case 'consultar':
+
+                break;
+            default:
+                console.log(` ERROR: parametro ${accion} no soportado `);        
+        }
+
+        break;
+    default:
+        console.log(` ERROR: parametro ${bdParam} no soportado `);
 }
 
-let main = async () => {
-    await cliente.connect();
-
-    console.log( ` - Empezó inserción:  ${ getHora() } ` );
-    
-    await llenarTablaPruebas(10)
-    
-    console.log( ` - Terminó inserción: ${ getHora() } ` );
-
-    await cliente.end();
-
-    return ' ==== Proceso terminado ==== ';
-}
-
-main()
-    .then( res => console.log(res) )
-    .catch( err => console.log(` >>>>>>> Error ${err}`) );
-
-/* [ FIN ] SCRIPT PARA POSTGRESQL */
 
 
